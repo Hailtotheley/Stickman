d0093cd5c95b9e4823aa88f685e9bd3432307e4f
Hailey <tanzhailey@gmail.com>
2025-08-02 10:29:37 +0800

    refactorisa

---
+++ Stickman/EmojiView.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiView.swift
+//  Stickman
+//
+//  Created by Eugene Tan on 2/8/25.
+//
+import SwiftUI
+struct EmojiView: View {
+    let symbol: String
+    let size: CGFloat
+    let x: CGFloat
+    let y: CGFloat
+    var body: some View {
+        Text(symbol)
+            .font(.system(size: size))
+            .offset(x: x, y: y)
+    }
+}
